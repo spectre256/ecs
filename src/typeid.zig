f515986e1bc4ca740e3d3ff6f19051a94402a665
Ellis Gibbons <egibbons256@gmail.com>
2025-12-31 17:06:36 -0600

    Fix typeid implementation

--- src/typeid.zig
+++ src/typeid.zig
@@ -4,31 +4,54 @@ const std = @import("std");
 const section = ".bss.typeids";
 const head: u8 align(1) linksection(section) = undefined;
 
+/// Returns a unique integer for every type passed to
+/// the function. It is idempotent. For compound types
+/// (struct, union), uniqueness is determined by
+/// declaration site. Thus, the same struct defined in
+/// two places will have a unique type id, whereas the
+/// same declaration will always be given the same type
+/// id. Anonymous structs are always unique.
 pub fn typeId(T: type) usize {
-    _ = head;
+    // Reference head to ensure that it is placed first
+    // Note that a simple discard will not work
+    const first = @intFromPtr(&head) + 1;
     return @intFromPtr(&struct {
         const _ = T;
         const index: u8 align(1) linksection(section) = undefined;
-    }.index) - @intFromPtr(&head) - 1;
+    }.index) - first;
 }
 
 const expect = std.testing.expect;
 
-fn U(V: type) type {
-    return struct { v: V };
-}
-test {
-    std.debug.print("u8 = {}, u16 = {}\n", .{ typeId(u8), typeId(u16) });
+test "basic types" {
     try expect(typeId(u8) == 0);
     try expect(typeId(u16) == 1);
     try expect(typeId(u8) == typeId(u8));
     try expect(typeId(u16) == typeId(u16));
     try expect(typeId(u8) != typeId(u16));
+    try expect(typeId(void) == typeId(void));
+}
+
+test "compound types" {
     const T = struct { field: []const u8 };
     try expect(typeId(T) == typeId(T));
+    const U = enum { tag1, tag2 };
+    try expect(typeId(U) == typeId(U));
+    const V = union { tag1: void, tag2: u8 };
+    try expect(typeId(V) == typeId(V));
+}
 
-    try expect(typeId(U(u8)) == typeId(U(u8)));
-    try expect(typeId(U(u8)) != typeId(U(u16)));
+test "generic types" {
+    const T = struct {
+        fn T(U: type) type {
+            return struct { u: U };
+        }
+    }.T;
+    try expect(typeId(T(u8)) == typeId(T(u8)));
+    try expect(typeId(T(u8)) != typeId(T(u16)));
+}
+
+test "anonymous types" {
     try expect(typeId(struct { field: []const u8 }) != typeId(struct { field: []const u8 }));
     try expect(typeId(struct { field1: []const u8 }) != typeId(struct { field2: []const u8 }));
 }
